2c84f1d5c703677c06e217d511eaad3ca82db42e
Pär Strindevall <par@strindevall.com>
2015-11-19 18:55:29 +0100

    Initial Commit

---
+++ base64encode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  base64encode
+//
+//  Created by Pär Strindevall on 201511/19/.
+//  Copyright © 2015 Pär Strindevall. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
